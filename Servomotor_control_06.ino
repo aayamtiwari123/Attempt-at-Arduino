b252001df54ebe1d9857b6447116f9157b7630c0
aayamtiwari123 <88884196+aayamtiwari123@users.noreply.github.com>
2025-08-13 13:35:42 +0545

    Servomotor_control_06.ino
    First attempt at servo motor

--- Servomotor_control_06.ino
+++ Servomotor_control_06.ino
@@ -1,3 +1,5 @@
+
+// basic servo control
 #include <Servo.h>
 
 Servo myServo;
@@ -14,3 +16,28 @@ void loop() {
   myServo.write(180);  // move to 180°
   delay(1000);
 }
+/*
+  to move servo motor faster
+
+  #include <Servo.h>
+
+  Servo myServo;
+
+  void setup() {
+    myServo.attach(9);
+  }
+
+  void loop() {
+    // sweep from 0° to 180°
+    for(int pos = 0; pos <= 180; pos += 5){ // bigger step = faster
+      myServo.write(pos);
+      delay(15); // shorter delay = faster
+    }
+    // sweep back
+    for(int pos = 180; pos >= 0; pos -= 5){
+      myServo.write(pos);
+      delay(15);
+    }
+  }
+*/
+
